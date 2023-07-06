6ce5a99c9e803ea372d8dd7820502d1f56d91552
fabian.zapata@sofka.com.co <fabian.zapata@sofka.com.co>
2023-07-06 17:34:56 -0500

    add IoC

--- MicroservicesRabbitMQ/MicroRabbit.Infra.Bus/RabbitMqBus.cs
+++ MicroservicesRabbitMQ/MicroRabbit.Infra.Bus/RabbitMqBus.cs
@@ -1,35 +1,38 @@
 ﻿using System.Text;
-using System.Text.Json.Serialization;
 using MediatR;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Domain.Core.Commands;
 using MicroRabbit.Domain.Core.Events;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace MicroRabbit.Infra.Bus
 {
     //sealed : que no permita herencia
     public sealed class RabbitMqBus : IEventBus
     {
+        private readonly RabbitMQSettings _settings;
         private readonly IMediator _mediator;
         private readonly Dictionary<string, List<Type>> _handlers;
         private readonly List<Type> _eventTypes;
 
-        public RabbitMqBus(IMediator mediator)
+        public RabbitMqBus(IMediator mediator, IOptions<RabbitMQSettings> settings)
         {
             _mediator = mediator;
             _handlers = new Dictionary<string, List<Type>>();
             _eventTypes = new List<Type>();
+            _settings = settings.Value;
         }
 
         public void Publish<T>(T @event) where T : Event
         {
             var factory = new ConnectionFactory
             {
-                HostName = "localhost",
-                UserName = "fazapatav",
-                Password = "as123456",
+                HostName = _settings.Hostname,
+                UserName = _settings.Username,
+                Password = _settings.Password
             };
 
             using (var connection = factory.CreateConnection())
@@ -52,7 +55,78 @@ namespace MicroRabbit.Infra.Bus
             where T : Event
             where TH : IEventHandler<T>
         {
-            throw new NotImplementedException();
+            var eventName = typeof(T).Name;
+            var handlerType = typeof(TH);
+
+            if (!_eventTypes.Contains(typeof(T)))
+            {
+                _eventTypes.Add(typeof(T));
+            }
+
+            if (!_handlers.ContainsKey(eventName)){
+                _handlers.Add(eventName, new List<Type>());
+            }
+
+            if (_handlers[eventName].Any(s => s.GetType() == handlerType))
+            {
+                throw new ArgumentException($"El handler exception {handlerType.Name} ya fue registrado anteriormente por {eventName}");
+            }
+
+            _handlers[eventName].Add(handlerType);
+            StartBasicConsume<T>();
+        }
+
+        private void StartBasicConsume<T>() where T : Event
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _settings.Hostname,
+                UserName = _settings.Username,
+                Password = _settings.Password,
+                DispatchConsumersAsync = true
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var eventName = typeof (T).Name;
+            channel.QueueDeclare(eventName,false,false,false,null);
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.Received += ConsumerReceived;
+
+            channel.BasicConsume(eventName, true, consumer);
+        }
+
+        private async Task ConsumerReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var eventName = e.RoutingKey;
+            var message = Encoding.UTF8.GetString(e.Body.Span);
+
+            try
+            {
+                await ProcessEvent(eventName, message).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private async Task ProcessEvent(string eventName, string message)
+        {
+            if (_handlers.ContainsKey(eventName))
+            {
+                var subscriptions = _handlers[eventName];
+                foreach ( var subscription in subscriptions)
+                {
+                    var handler = Activator.CreateInstance(subscription);
+                    if (handler == null) continue;
+                    var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+                    var @even = JsonConvert.DeserializeObject(message, eventType);
+                    var concretType = typeof(IEventHandler<>).MakeGenericType(eventType);
+                    await (Task)concretType.GetMethod("Handle").Invoke(handler, new object[] { even });
+                }
+            }
         }
     }
 }
---
+++ MicroservicesRabbitMQ/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public  class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IEventBus, RabbitMqBus>();
+            services.Configure<RabbitMQSettings>(c => configuration.GetSection("RabbitMQSettings"));
+        }
+    }
+}
